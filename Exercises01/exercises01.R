1e4c31a01817e60ace7331bc006c551536e23f2a
Spencer Woody <spencer.woody@utexas.edu>
2016-08-24 17:10:41 -0500

    Add  Exercises01/

--- Exercises01/exercises01.R
+++ Exercises01/exercises01.R
@@ -2,6 +2,18 @@
 ######### Created by Spencer Woody on 23 Aug 2016 #########
 ###########################################################
 
-#Cooode
+library(Matrix)
+library(microbenchmark)
+
+# No. 1 pt C
+
+# No. 1 pt D
+
+N <- 2000
+P <-  500
+
+X <- matrix(rnorm(N * P), nrow = N)
+mask <- matrix(rbinom(N * P, 1, 0.05), nrow = N)
+X <- mask * X
 
 # END
