5615543bb4e9e5b8ed230326b3cc64d2a6076086
Spencer Woody <spencer.woody@utexas.edu>
2016-08-26 14:16:50 -0500

    Add Exercises01/

--- Exercises01/exercises01.R
+++ Exercises01/exercises01.R
@@ -5,15 +5,55 @@
 library(Matrix)
 library(microbenchmark)
 
-# No. 1 pt C
+### No. 1 pt B
 
-# No. 1 pt D
+# Set N, P, X, W, and y
 
 N <- 2000
 P <-  500
 
 X <- matrix(rnorm(N * P), nrow = N)
-mask <- matrix(rbinom(N * P, 1, 0.05), nrow = N)
-X <- mask * X
+W <- diag(rep(1, N))
+y <- matrix(rnorm(N), nrow = N)
+
+# Inversion method
+
+bhat.Inv <- solve(t(X) %*% W %*% X) %*% t(X) %*% W %*% y
+
+
+# LU decomp method
+
+D <- t(X) %*% W %*% y
+C <- t(X) %*% W %*% X
+
+LUdecomp <- lu(C)
+L <- expand(LUdecomp)$L
+U <- expand(LUdecomp)$U
+
+A <- solve(L, D)
+bhat.LU <- solve(U, A)
+
+# Cholesky decomp method
+
+Cho <- chol(C)
+
+L.Cho <- t(Cho)
+U.Cho <- Cho
+
+A.Cho <- solve(L.Cho, D)
+bhat.Cho <- solve(U.Cho, A.Cho)
+
+
+
+### No. 1 pt C
+
+### No. 1 pt D
+
+# N <- 2000
+# P <-  500
+#
+# X <- matrix(rnorm(N * P), nrow = N)
+# mask <- matrix(rbinom(N * P, 1, 0.05), nrow = N)
+# X <- mask * X
 
 # END
