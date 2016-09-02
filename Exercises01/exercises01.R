65dd682e57e03ada666f5e915799e69263906e3c
Spencer Woody <spencer.woody@utexas.edu>
2016-09-01 21:25:55 -0500

    Add Exercises01

--- Exercises01/exercises01.R
+++ Exercises01/exercises01.R
@@ -9,8 +9,8 @@ library(microbenchmark)
 
 # Set N, P, X, W, and y
 
-N <- 2000
-P <-  500
+N <- 4000
+P <- 1000
 
 X <- matrix(rnorm(N * P), nrow = N)
 y <- matrix(rnorm(N), nrow = N)
@@ -41,47 +41,43 @@ Cho.decomp <- function(X.Cho, W.Cho, y.Cho) {
 microbenchmark(
 	Inv.method(X, W, y),
 	Cho.decomp(X, W, y),
-	times=5)
+	times = 5, unit = "ms") # N = 4000, P = 1000
 
 
 ### No. 1 pt D
 
 N <- 2000
-P <-  500
+P <- 1000
+
+# Sparsity measure # 0.01, 0.05, 0.25
+alpha <- 0.25
 
 X <- matrix(rnorm(N * P), nrow = N)
-mask <- matrix(rbinom(N * P, 1, 0.05), nrow = N)
+mask <- matrix(rbinom(N * P, 1, alpha), nrow = N)
 X <- mask * X
+W <- diag(rep(1, N))
 
-Inv.methodSPARSE <- function(X.Inv, W.Inv, y.Inv) {
-	X <- Matrix(X, sparse = TRUE)
-	XtWX <- (t(X.Inv)*diag(W.Inv)) %*% X.Inv
-	XtWY <- (t(X.Inv)*diag(W.Inv)) %*% y.Inv
-	bhat.Inv = Matrix::solve(XtWX, XtWY, sparse = TRUE)
-	return(bhat.Inv)
+
+Cho.decompSPARSE <- function(X.Cho, W.Cho, y.Cho) {
+	X.Cho <- Matrix(X.Cho, sparse = T)
+	D.Cho <- (t(X.Cho)*diag(W.Cho)) %*% y.Cho
+	C.Cho <- (t(X.Cho)*diag(W.Cho)) %*% X.Cho
+	
+	U.Cho <- chol(C.Cho)
+	L.Cho <- t(U.Cho)
+	
+	u <- forwardsolve(L.Cho, D.Cho)
+	bhat.Cho <- backsolve(U.Cho, u)
+	
+	return(bhat.Cho)
 }
 
-Inv.methodSPARSE2 <- function(X.Inv, W.Inv, y.Inv) {
-	XtWX <- (t(X.Inv)*diag(W.Inv)) %*% X.Inv
-	XtWY <- (t(X.Inv)*diag(W.Inv)) %*% y.Inv
-	bhat.Inv = solve(XtWX, XtWY)
-	return(bhat.Inv)
-}
+
 
 microbenchmark(
-	Inv.methodSPARSE(X, W, y),
-	Inv.methodSPARSE2(X, W, y), 
+	Inv.method(X, W, y),
 	Cho.decomp(X, W, y),
-	times=5)
-
-
-microbenchmark(
-	solve(XtWX, XtWY),
-	Matrix::solve(XtWX, XtWY, sparse = TRUE),
-	solve(XtWX) %*% XtWY,
-	times = 5
-)
-
-
+	Cho.decompSPARSE(X, W, y), 
+	times=5, unit = "ms") # N = 2000, P = 1000, alpha = 0.25
 
 # END
